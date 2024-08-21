4608d927f34643952b5fa2b8c8ecad1f818fde2c
Davier <daviersanbell@gmail.com>
2024-08-21 10:42:55 -0400

    directly deleting inactives

--- src/kademlia_network/time_heap.py
+++ src/kademlia_network/time_heap.py
@@ -3,23 +3,17 @@ class Time_Heap:
     def __init__(self):
         self.heap = deque()
         self.times_in_heap = {}
-        self.inactives = set()
     
     def add_vision(self, id):
         self.heap.append(id)
         self.times_in_heap[id] = self.times_in_heap.get(id, 0) + 1
-        if id in self.unactives:
-            self.inactives.remove(id)
     
     def get_least_seen(self):
         id = deque.popleft()
         while self.times_in_heap[id] != 1:
-            if id in self.inactives:
-                self.times_in_heap[id] = 0
-                return id
             self.times_in_heap[id] = min(self.times_in_heap[id] - 1, 0)
             id = deque.popleft()
-        self.times_in_heap[id] = 0
+        self.remove(id)
         return id
     
     def split(self, mid):
@@ -29,5 +23,5 @@ class Time_Heap:
             (left if id <= mid else right).add_vision(right)
         return (left, right)
     
-    def mark_as_inactive(self, id):
-        self.inactive.add(id)
+    def remove(self, id):
+        self.times_in_heap[id] = 0
