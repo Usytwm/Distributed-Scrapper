12280471e773d81016c9b0926ce5e661a5690b04
Davier <daviersanbell@gmail.com>
2024-08-25 22:43:23 -0400

    kademlia_queue_node

--- src/scrapper/scrapper_node.py
+++ src/scrapper/scrapper_node.py
@@ -9,31 +9,32 @@ from typing import List
 
 log = logging.getLogger(__name__)
 
+
 class Scrapper_Node:
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.node_data = NodeData(ip= self.host, port = self.port, type= 'scrapper')
+        self.node_data = NodeData(ip=self.host, port=self.port, type="scrapper")
         self.app = Flask(__name__)
         self.configure_endpoints()
-    
+
     def listen(self):
         def run_app():
             self.app.run(host=self.host, port=self.port, threaded=True)
 
         thread = Thread(target=run_app)
         thread.start()
-    
+
     def stop(self):
         pass
 
-    def register(self, entry_points : List[NodeData]):
+    def register(self, entry_points: List[NodeData]):
         for entry_point in entry_points:
-            node_address = str(entry_point.ip) + ':' + str(entry_point.port)
+            node_address = str(entry_point.ip) + ":" + str(entry_point.port)
             url = f"http://{node_address}/register"
-            data = self.node_data.to_json()
+            data = {"node": self.node_data.to_json()}
             try:
-                response = requests.post(url, json= data)
+                response = requests.post(url, json=data)
                 response.raise_for_status()
                 break
             except:
@@ -42,10 +43,10 @@ class Scrapper_Node:
     def configure_endpoints(self):
         @self.app.route("/scrap")
         def scrap():
-            data = request.get_json(force= True)
+            data = request.get_json(force=True)
             url = data.get("url")
             response = self.scrap(url)
             return jsonify(response), 200
-    
-    def scrap(self, url : str):
+
+    def scrap(self, url: str):
         pass
--- src/storage/storage_node.py
+++ src/storage/storage_node.py
@@ -4,61 +4,62 @@ from kademlia_network.kademlia_node import KademliaNode
 from typing import List
 from Interfaces.NodeData import NodeData
 
+
 class StorageNode:
     def __init__(self, host, port):
-        self.node_data = NodeData(ip= host, port= port, type = 'storage')
-        self.node = KademliaNode(ip= host, port= port)
+        self.node_data = NodeData(ip=host, port=port, type="storage")
+        self.node = KademliaNode(ip=host, port=port)
         self.app = self.node.app
         self.extend_endpoint()
-    
+
     def listen(self):
         self.node.listen()
-    
+
     def stop(self):
         self.node.stop()
-    
-    def register(self, entry_points : List[NodeData]):
+
+    def register(self, entry_points: List[NodeData]):
         for entry_point in entry_points:
-            node_address = str(entry_point.ip) + ':' + str(entry_point.port)
+            node_address = str(entry_point.ip) + ":" + str(entry_point.port)
             url = f"http://{node_address}/register"
-            data = self.node_data.to_json()
+            data = {"node": self.node_data.to_json()}
             try:
-                response = requests.post(url, json= data)
+                response = requests.post(url, json=data)
                 response.raise_for_status()
                 break
             except:
                 continue
-    
+
     def extend_endpoint(self):
         @self.app.route("/global/ping", methods=["GET"])
         def global_ping():
             response = self.global_ping()
             return jsonify(response), 200
-        
-        @self.app.route("/set", methods= ["POST"])
+
+        @self.app.route("/set", methods=["POST"])
         def set():
-            data = request.get_json(force= True)
+            data = request.get_json(force=True)
             key, value = data.get("key"), data.get("value")
             response = self.set(key, value)
             return jsonify(response), 200
-        
-        @self.app.route("/get", methods= ["GET"])
+
+        @self.app.route("/get", methods=["GET"])
         def get():
-            data = request.get_json(force= True)
+            data = request.get_json(force=True)
             key = data.get("key")
             response = self.get(key)
             return jsonify(response), 200
-    
+
     def global_ping(self):
-        return {'status' : 'OK'}
-    
+        return {"status": "OK"}
+
     def set(self, key, value):
         self.node.set(key, value)
-        return {'status' : 'OK'}
-    
+        return {"status": "OK"}
+
     def get(self, key):
         value = self.node.get(key)
         if not (value == False):
-            return {'status' : 'OK', 'value' : value}
+            return {"status": "OK", "value": value}
         else:
-            return {'status' : 'OK', 'value' : None}
+            return {"status": "OK", "value": None}
